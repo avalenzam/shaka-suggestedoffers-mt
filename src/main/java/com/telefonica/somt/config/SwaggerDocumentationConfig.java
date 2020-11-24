bf8668067154dae9c5cd315897b890582d90358e
avalenza <avalenza@indracompany.com>
2020-11-24 11:08:00 -0500

    [FEAT] modificacion del pom

--- src/main/java/com/telefonica/somt/config/SwaggerDocumentationConfig.java
+++ src/main/java/com/telefonica/somt/config/SwaggerDocumentationConfig.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-07-17T17:23:53.051Z")
 /**
@@ -20,18 +20,19 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @version: 0.1
  * @Description: Configuracion del Swagger
  */
+
+@EnableSwagger2
 @Configuration
 public class SwaggerDocumentationConfig {
 
     ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-            .title("Product Catalog Management - DRAFT VERSION")
-            .description("This is a sample representation of the Product Catalog T-Open API V2, which is used by applications to obtain information about the list of products that can be offered to customers as well as to obtain information about the relationship and possible bundles to offer different products together")
-            .license("")
-            .licenseUrl("http://unlicense.org")
+            .title("Suggested Offers MT")
+            .description("Motor de ofertas")
+            .license("Apache 2.0")
+            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
             .termsOfServiceUrl("")
-            .version("2.5.0")
-            .contact(new Contact("","", ""))
+            .version("1.0.0")
             .build();
     }
 
@@ -39,11 +40,10 @@ public class SwaggerDocumentationConfig {
     public Docket customImplementation(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                    .apis(RequestHandlerSelectors.basePackage("io.swagger.api"))
+                    .apis(RequestHandlerSelectors.basePackage("com.telefonica.somt"))
                     .build()
                 .directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
                 .directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class)
                 .apiInfo(apiInfo());
     }
-
 }
